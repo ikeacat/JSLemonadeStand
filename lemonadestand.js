f881c0da908a10732cb094671a842346b2dcf4a6
ikeacat <mfryk268@gmail.com>
2020-03-14 19:13:02 -0500

    Cool new buttons & hover stuff

--- lemonadestand.js
+++ lemonadestand.js
@@ -12,7 +12,7 @@ function startGame() {
   document.getElementById("rootDIV").innerHTML = spawnSmallHeader() + `<center><h1 class='robotoFont' style='font-size:40'>Stand Setup</h1><br>
   <span class="robotoFont"><p>Stand Name <input id='standnameinput'></input></p></span><br>
   <p class="robotoFont" style="color:red;">WARNING!!! This game does not yet save. Sorry bout that.</p><br>
-  <button class="defaultbutton robotoFont" onclick="startDay(0)">Save & Start Game</button></center>`;
+  <button class="neuButton robotoFont" onclick="startDay(0)">Save & Start Game</button></center>`;
   console.log("Loaded new game.");
 }
 
@@ -23,6 +23,7 @@ function startDay(num) {
     localStorage.setItem("money","0.50")
     var standnameval = document.getElementById("standnameinput").value;
     localStorage.setItem("standname",standnameval);
+    localStorage.setItem("ppl","0")
     document.getElementById("rootDIV").innerHTML = spawnSmallHeader() + `<div class="robotoFont"><p class='headertext'>Intro</p>
     <p>Welcome to Lemonsville, California!</p><br>
     <p>At the start, you will have to make a few decisions a day (you can unlock more through research):<br></p>
@@ -33,12 +34,13 @@ function startDay(num) {
     <br>
     <p>Your mom has decided to provide you with 50 cents to start out your business.</p>
     <p>For now... Thats it!</p><br>
-    <button class='defaultbutton' onclick='homeMenu()'>Alright...</button></div>`;
+    <button class='neuButton' onclick='homeMenu()'>Alright...</button></div>`;
   }
 }
 
 function startNewDay() {
   var lsday = localStorage.getItem("day")
+  var lsppl = localStorage.getItem("ppl")
   document.getElementById("rootDIV").innerHTML = "" // Clear Screen
   document.getElementById("rootDIV").innerHTML = `<div class='weatherReportCardDynamic defaultCardDynamic vertSizableCard robotoFont'>
   <div class='vertSizableCard longInsideDCD leftfifty'><h1>LEMONVILLE WEATHER REPORT</h1></div>
@@ -135,30 +137,18 @@ function startNewDay() {
   }
   document.getElementById("rootDIV").innerHTML += HSBreak() + `<div id='dayInputCard' class='defaultCardDynamic robotoFont vertSizableCard'><h1 class='leftfifty'>DAILY REPORT</h1>
   <br><div class='leftfifty'>
-  <p id='sugarprice'>Price to buy sugar is </p><br>
-  <p id='lemonprice'>Price to buy lemons is </p><br>
-  <p id='waterprice'>Price to get water is </p><br>
-  <p id='cupprice'>Price to buy plastic cups are </div></div>`;
+  <p id="ppl">Price per glass of lemonade is </p>
+  </div></div>`;
   if(lsday == "1") {
-    document.getElementById("sugarprice").innerHTML += "$0. (Your mom has decided to give you leftover sugar from baking)";
-    document.getElementById("lemonprice").innerHTML += "$0. (Your mom has decided to give you the lemons served with her cocktails)";
-    document.getElementById("waterprice").innerHTML += "$0. (Your house has free running water)";
-    document.getElementById("cupprice").innerHTML += "$0.01. (Your neighbor is charging a pretty penny for each of her plastic cups stolen from her Office Christmas party)";
-    localStorage.setItem("sugarprice","0");
-    localStorage.setItem("waterprice","0");
-    localStorage.setItem("lemonprice","0");
-    localStorage.setItem("cupprice","0.01");
+    document.getElementById("ppl").innerHTML += "$0 (your mom decided to help you get started).";
   } else {
-    var cp = localStorage.getItem("cupprice");
-    var wp = localStorage.getItem("waterprice");
-    var lp = localStorage.getItem("lemonprice");
-    var sp = localStorage.getItem('sugarprice');
-    document.getElementById("sugarprice").innerHTML += "$" + sp + ".";
-    document.getElementById("lemonprice").innerHTML += "$" + lp + ".";
-    document.getElementById("waterprice").innerHTML += "$" + wp + ".";
-    document.getElementById("cupprice").innerHTML += "$" + cp + ".";
+    document.getElementById("ppl").innerHTML += "$" + lsppl + ".";
   }
-  document.getElementById("dayInputCard").innerHTML += "<br><br><h2>Amount of sugar to put in 1 cup</h2>"
+  document.getElementById("dayInputCard").innerHTML += `<br><h2 class='leftfifty'>Amount of cups to make</h2>
+  <input id='amountofcups' class='leftfifty'></input><br>
+  <h2 class='leftfifty'>Price of a cup.</h2>
+  <input id='priceoflem' class='leftfifty'></input><br><br>
+  <button class='neuButton leftfifty'>Ok</button><br><br>`
 }
 
 function spawnSmallHeader() {
@@ -166,13 +156,12 @@ function spawnSmallHeader() {
 }
 
 function homeMenu() {
-  localStorage.setItem("basicThreads","true")
   document.getElementById("rootDIV").classList.remove("startrootdiv") // Free up space
   document.getElementById("rootDIV").innerHTML = "" // Clear HTML
   document.getElementById("rootDIV").innerHTML = InfoBlock() + HSBreak() + "<a href='javascript:startNewDay()'><div class='homeScreenCard defaultCardDynamic sndCard robotoFont'><div class='HSCardText'><h1>Start Day</h1><h4>Start the day (you cannot come back to this screen once day is started!)</h4></div></div></input>"; // FIRST TWO CARDS
   document.getElementById("rootDIV").innerHTML += HSBreak() + "<div class='homeScreenCard defaultCardDynamic robotoFont HSResearchCard'><div class='HSCardText HSResearchTextSize'><h1>Research</h1></div></div>" // Research Block
   document.getElementById("rootDIV").innerHTML += ResearchUnlock(); // Cards unlocked with research (if any, idk yet)
-  document.getElementById("rootDIV").innerHTML += "<br><br>" + version(6)
+  document.getElementById("rootDIV").innerHTML += "<br><br>" + version(6);
 }
 
 function InfoBlock() {
@@ -199,6 +188,6 @@ function version(sz) {
 }
 
 function ResearchUnlock() {
-  return "";
   console.log("No research tree yet :(.");
+  return "";
 }
